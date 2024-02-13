a99d0dd3722787458c62ab96c488974a0482f493
lohrenzo <lorenzofblack@gmail.com>
2024-02-13 20:25:49 +0000

    build file commit

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,10 @@
 # build_files.sh
-pipenv install -r requirements.txt
-python manage.py collectstatic
+
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
